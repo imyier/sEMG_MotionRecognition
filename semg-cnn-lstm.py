89fd1b4f6b4024ca1e9ab38754213f5182438e71
imyier <lizhen.jnu@gmail.com>
2018-10-24 22:05:56 +0800

    add cnn-lstm code

---
+++ semg-cnn-lstm.py
@@ -0,0 +1,16 @@
+import os
+
+import utils.utilities
+
+import tensorflow as tf
+import matplotlib
+import numpy
+
+# 数据读取与预处理
+```
+    从数据库中读取数据 train
+    表示训练集，test表示训练集
+    x表示输入，y label 为标签，即希望得到的输出
+```
+
+
